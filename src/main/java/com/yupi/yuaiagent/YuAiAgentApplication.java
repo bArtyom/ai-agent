aa07c45b8c028b1e48f92d610eccff5bd1695c89
bArtyom <2497416595@qq.com>
2025-11-03 15:34:53 +0800

    advisor实现

--- src/main/java/com/yupi/yuaiagent/YuAiAgentApplication.java
+++ src/main/java/com/yupi/yuaiagent/YuAiAgentApplication.java
@@ -1,9 +1,7 @@
 package com.yupi.yuaiagent;
 
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class YuAiAgentApplication {
@@ -12,15 +10,5 @@ public class YuAiAgentApplication {
         SpringApplication.run(YuAiAgentApplication.class, args);
     }
 
-    // 方式1：使用构造器注入
-    @Service
-    public class ChatService {
-        private final ChatClient chatClient;
-
-        public ChatService(ChatClient.Builder builder) {
-            this.chatClient = builder
-                    .defaultSystem("你是恋爱顾问")
-                    .build();
-        }
-    }
+    // 移除 ChatService，因为 LoveApp 已经提供了类似功能
 }
