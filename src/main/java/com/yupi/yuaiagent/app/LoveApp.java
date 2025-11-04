6e783b57b85bd9f3390b528bcfacdb66b38bb3e1
bArtyom <2497416595@qq.com>
2025-11-04 17:32:15 +0800

    生成json返回数据

--- src/main/java/com/yupi/yuaiagent/app/LoveApp.java
+++ src/main/java/com/yupi/yuaiagent/app/LoveApp.java
@@ -1,6 +1,8 @@
 package com.yupi.yuaiagent.app;
 
 import com.yupi.yuaiagent.advisor.MyLoggerAdvisor;
+import com.yupi.yuaiagent.advisor.ReReadingAdvisor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -9,6 +11,8 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
@@ -31,7 +35,7 @@ public class LoveApp {
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
                         new MessageChatMemoryAdvisor(chatMemory),
-                        new MyLoggerAdvisor()
+                        new ReReadingAdvisor()
                 )
                 .build();
     }
@@ -48,4 +52,33 @@ public class LoveApp {
         return content;
     }
 
+    record LoveReport(String title, List<String> suggestions) {
+    }
+
+    public LoveReport doChatWithReport(String message,String chatId){
+        String formatInstruction = """
+               你必须生成符合下面格式的回答：
+               使用Json格式，例如
+                {"title": "恋爱报告：程序员鱼皮的爱情指南",
+                  "suggestions": [
+                       "拓展社交圈：积极参与社区活动、线上社群，扩大社交圈子。"
+                       "提升个人魅力：注重仪容仪表、提升内在修养，展现个人魅力。"
+                        "培养共同兴趣：找到共同爱好，增加互动和话题，培养情感联系。"
+                      "练习沟通技巧：清晰表达需求，积极倾听，表达真挚情感。"
+                      "寻求专业帮助：考虑心理咨询师，更好地了解自我和爱情。"
+                    ]
+                 }
+                """;
+        
+        LoveReport loveReport=chatClient
+                .prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为用户名的恋爱报告，内容为建议列表。" + formatInstruction)
+                .user(message)
+                .advisors(spec-> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY,chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY,10))
+                .call()
+                .entity(LoveReport.class);
+        log.info("loveReport: {}", loveReport);
+        return loveReport;
+    }
 }
